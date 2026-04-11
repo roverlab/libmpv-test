6375485a01f8e8a106e18b0859956b2df2763125
palidin <palidin@foxmail.com>
2026-04-11 21:56:10 +0800

    1

--- build.sh
+++ build.sh
@@ -87,7 +87,10 @@ for ARCH in $ARCHS; do
 				mkdir -p $SCRATCH/$ARCH/freetype && cd $_ && $SCRIPTS/freetype-build
 				;;
             "libharfbuzz" )
-				mkdir -p $SCRATCH/$ARCH/harfbuzz && cd $_ && $SCRIPTS/harfbuzz-build
+				# Disable cast-function-type-strict warning for compatibility with older harfbuzz
+				HB_CFLAGS="$CFLAGS -Wno-error=cast-function-type-strict"
+				HB_CXXFLAGS="$CXXFLAGS -Wno-error=cast-function-type-strict"
+				mkdir -p $SCRATCH/$ARCH/harfbuzz && cd $_ && CFLAGS="$HB_CFLAGS" CXXFLAGS="$HB_CXXFLAGS" $SCRIPTS/harfbuzz-build
 				;;
             "libass" )
 				mkdir -p $SCRATCH/$ARCH/libass && cd $_ && $SCRIPTS/libass-build
