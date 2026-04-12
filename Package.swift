42947875b0768eef32b406632153517674704079
palidin <palidin@foxmail.com>
2026-04-13 00:49:30 +0800

    3

--- Package.swift
+++ Package.swift
@@ -17,7 +17,18 @@ let package = Package(
         .binaryTarget(
             name: "Libmpv",
             url: "https://github.com/roverlab/libmpv-test/releases/download/v0.1.29/Libmpv.xcframework.zip",
-            checksum: "1857ee9eea56d5dd2d65097062fc178b72a10c76f4c2daf8bb2bc29a4d24ec44"
+            checksum: "1857ee9eea56d5dd2d65097062fc178b72a10c76f4c2daf8bb2bc29a4d24ec44",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("VideoToolbox"),
+                .linkedLibrary("bz2"),
+                .linkedLibrary("z"),
+                .linkedLibrary("iconv"),
+                .linkedLibrary("c++"),
+            ]
         )
     ]
 )
