2f9f5cb72222e9ff5a61a942337a09b08dcff6ca
palidin <palidin@foxmail.com>
2026-04-12 14:30:11 +0800

    1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -56,7 +56,7 @@ let package = Package(
             cSettings: [
                 .headerSearchPath("../Sources/Libmpv/include")
             ],
-            resources: .copy("Info.plist")
+            resources: [.copy("Info.plist")]
         )
     ]
 )
