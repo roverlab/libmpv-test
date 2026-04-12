0fb1b26c897c0852b7575b6ec75ce10d12877b30
palidin <palidin@foxmail.com>
2026-04-12 20:33:15 +0800

    3

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Libmpv",
-            url: "https://github.com/roverlab/libmpv-test/releases/download/v0.1.8/Libmpv.xcframework.zip",
+            url: "https://github.com/roverlab/libmpv-test/releases/download/v0.1.22/Libmpv.xcframework.zip",
             checksum: "4d46388df8c6d17d9b51416ec1537478a44ac0a4a52ac5d00f89906db0d77061"
         )
     ]
